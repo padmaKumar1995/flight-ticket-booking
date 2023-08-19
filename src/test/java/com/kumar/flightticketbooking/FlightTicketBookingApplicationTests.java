006d78cb8718ab1ce6722b55cae37c25b31cf2fc
Padmakumar Ariharasubramanian <padmakumar0930@Padmakumars-MacBook-Air.local>
2023-08-20 00:17:10 +0530

    flight ticket booking project created

---
+++ src/test/java/com/kumar/flightticketbooking/FlightTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumar.flightticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
